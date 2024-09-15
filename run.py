c758a71ed5c042c9c252beca2728a69ec5a1585d
Bugra Kilic <bugrakilic@outlook.com>
2024-09-15 22:38:39 +0300

    Added new flask routes

--- run.py
+++ run.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# ip calculator route - added 1509
+@app.route('/ip_calculator')
+def ip_calculator():
+    return render_template('ip_calculator.html')
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
