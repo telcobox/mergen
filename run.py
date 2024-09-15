b7817085b0ff2f33d7c3c2563b84961e4656ebaf
Bugra Kilic <bugrakilic@outlook.com>
2024-09-15 22:40:27 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,11 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# ip calculator route - added 1509
-@app.route('/ip_calculator')
-def ip_calculator():
-    return render_template('ip_calculator.html')
-
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-
