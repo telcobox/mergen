517c0d3f767d28bd397bf8df98aa2ec4a52c7cc1
Bugra Kilic <bugrakilic@outlook.com>
2024-09-18 22:40:59 +0300

    Revised the code for redirections

--- run.py
+++ run.py
@@ -8,19 +8,19 @@ def home():
 
 @app.route('/dns-lookup')
 def dnslookup():
-    return render_template('/dns_lookup.html')
+    return render_template('dns_lookup.html')
 
 @app.route('/ipsubnet-calculator')
 def ipsubnetcalculator():
-    return render_template('/ip_calculator.html')
+    return render_template('ip_calculator.html')
 
 @app.route('/ping-latency-checker')
 def pinglatencychecker():
-    return render_template('/ping_latency.html')
+    return render_template('ping_latency.html')
 
 @app.route('/whats-my-ip')
 def whatsmyip():
-    return render_template('/whats_my_ip.html')
+    return render_template('whats_my_ip.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
